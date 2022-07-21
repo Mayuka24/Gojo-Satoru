375d28dfee32c3ff341f6d08cd273e92ed42c2ca
Mayuka24 <109749318+Mayuka24@users.noreply.github.com>
2022-07-21 20:49:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['94764020947']
+global.premium = ['94764020947']
+global.ownernomer = '94764020947'
+global.ownername = 'Ｍ_ᴘɪʏᴜᵐᴬɴ~'
+global.botname = 'Ｍ_ᴘɪʏᴜᵐᴬɴ~'
+global.footer = 'Ｍ_ᴘɪʏᴜᵐᴬɴ~'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'Ｍ_ᴘɪʏᴜᵐᴬɴ~'
+global.author = 'Ｍ_ᴘɪʏᴜᵐᴬɴ~'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
